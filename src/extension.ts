e6e9bea735ab539774a02b03c2d4c17bc2b0fe17
Kanmii <maneptha@gmail.com>
2022-09-27 07:06:52 +0200

    update node packages

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,63 @@
-import path from 'path'
-import { TransportKind, ExtensionContext, LanguageClient, ServerOptions, commands, window, workspace, services, languages, LanguageClientOptions } from 'coc.nvim'
-import { DockerComposeCompletionItemProvider } from './dockerCompose/dockerComposeCompletionItemProvider'
+import path from "path";
+import {
+  TransportKind,
+  ExtensionContext,
+  LanguageClient,
+  ServerOptions,
+  commands,
+  window,
+  workspace,
+  services,
+  languages,
+  LanguageClientOptions,
+} from "coc.nvim";
+import { DockerComposeCompletionItemProvider } from "./dockerCompose/dockerComposeCompletionItemProvider";
 
 interface DockerConfig {
-  enable: boolean
+  enable: boolean;
 }
 
 export async function activate(context: ExtensionContext): Promise<void> {
-
-  const config = workspace.getConfiguration().get('docker', {}) as DockerConfig
+  const config = workspace.getConfiguration().get("docker", {}) as DockerConfig;
   if (config.enable === false) {
-    return
+    return;
   }
 
-  const serverModule = require.resolve('dockerfile-language-server-nodejs/lib/server.js')
+  const serverModule = require.resolve(
+    "dockerfile-language-server-nodejs/lib/server.js"
+  );
 
   const serverOptions: ServerOptions = {
     module: serverModule,
     transport: TransportKind.ipc,
-    args: ["--node-ipc"]
-  }
+    args: ["--node-ipc"],
+  };
 
   const clientOptions: LanguageClientOptions = {
-    documentSelector: ['Dockerfile', 'dockerfile']
-  }
+    documentSelector: ["Dockerfile", "dockerfile"],
+  };
 
-  const client = new LanguageClient("docker", "dockerfile-language-server-nodejs", serverOptions, clientOptions)
+  const client = new LanguageClient(
+    "docker",
+    "dockerfile-language-server-nodejs",
+    serverOptions,
+    clientOptions
+  );
 
   context.subscriptions.push(
     services.registLanguageClient(client),
     commands.registerCommand("docker.version", async () => {
-      const v = require(path.resolve(__dirname, '..', 'package.json')).version
-      window.showMessage(`Version: ${v}`, 'more')
+      const v = require(path.resolve(__dirname, "..", "package.json")).version;
+      window.showMessage(`Version: ${v}`, "more");
     })
-  )
+  );
 
   context.subscriptions.push(
-    languages.registerCompletionItemProvider('docker-compose', 'docker', 'yaml.docker-compose', new DockerComposeCompletionItemProvider())
-  )
+    languages.registerCompletionItemProvider(
+      "docker-compose",
+      "docker",
+      "yaml.docker-compose" as any,
+      new DockerComposeCompletionItemProvider()
+    ) as any
+  );
 }
-
